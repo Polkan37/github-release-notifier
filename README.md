ef86948d5ec0cb5b59db682df2b9a74fdf25f7a9
Polkan37 <zaq2587413699@gmail.com>
2026-04-09 15:25:48 +0300

    feat: basic init

---
+++ README.md
@@ -0,0 +1,8 @@
+# GitHub Release Notifier
+
+API that allows users to subscribe to email notifications about new releases of a chosen GitHub repository.
+
+
+## Run
+docker-compose up -d
+npm run dev
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { buildApp } from './app'
+
+const app = buildApp()
+
+app.listen({ port: 3000 }, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log('Server running on http://localhost:3000')
+})
