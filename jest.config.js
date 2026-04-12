6ca6d2e88fa47da5557b68f606e4a1cade3fdf25
Polkan37 <zaq2587413699@gmail.com>
2026-04-13 00:05:34 +0300

    update yml file

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
 
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+
   testPathIgnorePatterns: [
     '/node_modules/',
     '/dist/',
